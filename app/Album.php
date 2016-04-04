6a38f5b9e1bdaac290bb0d6e02fa50f2af9a84f3
Robert Calin <u1072113@unimail.hud.ac.uk>
2016-04-05 00:59:01 +0100

    Create model
    Here, I have created the models tables for Album and Photo with the
    composer php artisan.
    php artisan make:model Album
    php artisan make:model Photo

---
+++ app/Album.php
@@ -0,0 +1,9 @@
+<?php namespace PhotoShots;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Album extends Model {
+
+	//
+
+}
---
+++ app/Photo.php
@@ -0,0 +1,9 @@
+<?php namespace PhotoShots;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model {
+
+	//
+
+}
