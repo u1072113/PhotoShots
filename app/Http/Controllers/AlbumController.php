739f388c58ebe3421c832ef7c2f3e6fa89c17c16
Robert Calin <u1072113@unimail.hud.ac.uk>
2016-04-05 01:46:42 +0100

    Created the controllers
    I have created 3 controllers in composer: UserController,
    AlbumController and PhotoController.
    
    php artisan make:controller UserController  --plain
    php artisan make:controller UserController  --plain
    php artisan make:controller UserController  --plain
    
    I have used the --plain parameter to avoid laravel to create many
    functions that are not needed.

---
+++ app/Http/Controllers/AlbumController.php
@@ -0,0 +1,12 @@
+<?php namespace PhotoShots\Http\Controllers;
+
+use PhotoShots\Http\Requests;
+use PhotoShots\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class AlbumController extends Controller {
+
+	//
+
+}
