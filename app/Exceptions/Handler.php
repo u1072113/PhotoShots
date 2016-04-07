28fb7423214c2b1003e90ac58cd871353706e967
Robert Calin <u1072113@unimail.hud.ac.uk>
2016-04-07 05:10:44 +0100

    Issues with handlling errors
    not solved yet, still working on it

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,42 +1,68 @@
 <?php namespace PhotoShots\Exceptions;
 
   use Exception;
-  use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
- +use Symfony\Component\HttpKernel\Exception\HttpException;
-  
-  class Handler extends ExceptionHandler {
-  
- @@ -36,7 +37,32 @@ public function report(Exception $e)
-  	 */
-  	public function render($request, Exception $e)
-  	{
- -		return parent::render($request, $e);
- +		if(config('app.debug'))
- +		{
- +			return parent::render($request, $e);
- +		}
- +		elseif ($this->isHttpException($e))
- +		{
- +			return $this->renderHttpException($e);
- +		}
- +		else
- +		{
- +			return redirect('/')->withErrors('Unexpected Error. Try later.');
- +		}
- +	}
- +
- +	protected function renderHttpException(HttpException $e)
- +	{
- +		$status = $e->getStatusCode();
- +
- +		if (view()->exists("errors.{$status}"))
- +		{
- +			return response()->view("errors.{$status}", [], $status);
- +		}
- +		else
- +		{
- +			return response()->view("errors.default", [], $status);
- +		}
-  	}
-  
-  }
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+class Handler extends ExceptionHandler {
+	/**
+	 * A list of the exception types that should not be reported.
+	 *
+	 * @var array
+	 */
+	protected $dontReport = [
+	    AuthorizationException::class,
+	    HttpException::class,
+	    ModelNotFoundException::class,
+	    ValidationException::class,
+	];
+	/**
+	 * Report or log an exception.
+	 *
+	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+	 *
+	 * @param  \Exception  $e
+	 * @return void
+	 */
+	public function report(Exception $e)
+	{
+		return parent::report($e);
+	}
+	/**
+	 * Render an exception into an HTTP response.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Exception  $e
+	 * @return \Illuminate\Http\Response
+	 */
+	public function render($request, Exception $e)
+	{
+		if(config('app.debug'))
+		{
+			return parent::render($request, $e);
+		}
+		elseif ($this->isHttpException($e))
+		{
+			return $this->renderHttpException($e);
+		}
+		else
+		{
+			return redirect('/')->withErrors('Unexpected Error. Try later.');
+		}
+	}
+	protected function renderHttpException(HttpException $e)
+	{
+		$status = $e->getStatusCode();
+		if (view()->exists("errors.{$status}"))
+		{
+			return response()->view("errors.{$status}", [], $status);
+		}
+		else
+		{
+			return response()->view("errors.default", [], $status);
+		}
+	}
+}
