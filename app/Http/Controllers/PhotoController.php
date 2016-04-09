e921864646cfee5a37a4211793512abcb2b95c43
Robert Calin <u1072113@unimail.hud.ac.uk>
2016-04-09 22:17:18 +0100

    Showing Photos
    When clicking show photo on an album, the photos will show

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -5,16 +5,23 @@ use PhotoShots\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
+use PhotoShots\Http\Requests\ShowPhotosRequest;
+
+use PhotoShots\Album;
+use PhotoShots\Photo;
+
 class PhotoController extends Controller {
 
 		public function __construct()
 	{
 		$this->middleware('auth');
 }
-
-	public function getIndex()
+// Here we imported the motheod for the photos to show on the album page.
+	public function getIndex(ShowPhotosRequest $request)
 	{
-		return 'Showing all the album photos';
+		$photos = Album::find($request->get('id'))->photos;
+
+		return view('photos.show',['photos' => $photos, 'id' => $request->get('id')]);
 	}
 
 	public function getCreatePhoto()
---
+++ app/Http/Requests/ShowPhotosRequest.php
@@ -0,0 +1,41 @@
+<?php namespace PhotoShots\Http\Requests;
+
+use PhotoShots\Http\Requests\Request;
+
+use Auth;
+class ShowPhotosRequest extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		$user = Auth::user();
+		$id = $this->get('id');
+//This will find the album with the user ID which is linked to.
+		$album = $user->albums()->find($id);
+
+		if($album)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+		
+			'id' => 'required'
+
+		];
+	}
+
+}
